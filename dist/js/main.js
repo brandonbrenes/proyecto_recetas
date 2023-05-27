26e6591d5426ecf1a8fa6f544cc3389d30c00ccd
Brandon Josué Brenes Arias <brandon.brenes@ucr.ac.cr>
2023-05-26 22:35:12 -0600

    Se agregó la lista de las categorías que se se carga con datos de la api y se agregaron en el html en los botones de filtrar

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,12 +1,31 @@
 const app = Vue.createApp({
     data() {
         return {
+            categories: [],
             selectedId: "53058",
             recipesIds: [],//solo guarda el id de las recetas
             recetasColeccionadas: []
         }
     },
     mounted: function() {
+        axios({
+            method: 'get',
+            url: 'https://www.themealdb.com/api/json/v1/1/list.php?c=list'
+        })
+        .then(
+            (response) => {
+                let items= response.data.meals;
+                
+                items.forEach(item => {
+                    this.categories.push(item.strCategory);
+                });
+                //this.recipesIds.push(id);
+            }
+        )
+        .catch(
+            error => console.log(error)
+        )
+
         for(let i=0; i<16; i++){
 
             axios({
@@ -32,5 +51,3 @@ const app = Vue.createApp({
 
 const emitter = mitt();
 app.config.globalProperties.$test = emitter;
-//app.config.globalProperties.$eventBus = emitter;
-app.use(router);
